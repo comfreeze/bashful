a51825d98deed2f3ff23f6f9537afd492a84730d
James Zimmerman II <james@impekable.com>
2017-12-08 16:22:49 -0500

    Adding generic ASCII color support.

---
+++ ascii.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#
+# CONFIG
+###################
+_ESCS='\[\033'
+_ESCE='\]'
+
+#
+# CUSTOM LIBRARIES
+###################
+require platform
+
+#
+# MODULE LOGIC
+###################

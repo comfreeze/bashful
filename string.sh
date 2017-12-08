693560720f3980d7ad6f1ad234876f9bece87bbc
James Zimmerman II <james@impekable.com>
2017-12-08 16:36:30 -0500

    Improved string repeater logic.

--- string.sh
+++ string.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 #
-# String utilities
+# CONFIG
+###################
+
 #
-in_string () {
+# MODULE LOGIC
+###################
+
+#
+# String utilities
+###################
+# Test if char sequence in string
+function in_string ()
+{
 #  dump_method $*
   local t;  t=${1};  shift
   local s;  s=${1};  shift
@@ -11,24 +21,26 @@ in_string () {
   echo "true";
 }
 export -f in_string
-function real_length() {
+# Calculate length without special chars
+function real_length ()
+{
   dump_method $*
-    local LENGTH1; LENGTH1=$(echo "$1" | awk '{ print length }')
-    local LENGTH2; LENGTH2=$(echo "${#1}")
-    local LENGTH3; LENGTH3=$(expr length "${1}")
-    # echo "$LENGTH1 - $LENGTH2 - $LENGTH3"
-    if [ "$LENGTH1" -le "$LENGTH2" ] && [ "$LENGTH1" -le "$LENGTH3" ]; then
-        echo "$LENGTH1";
-    elif [ "$LENGTH2" -le "$LENGTH1" ] && [ "$LENGTH2" -le "$LENGTH3" ]; then
-        echo "$LENGTH2";
-    else
-        echo "$LENGTH3";
-    fi
+  local LENGTH1; LENGTH1=$(echo "$1" | awk '{ print length }')
+  local LENGTH2; LENGTH2=$(echo "${#1}")
+  local LENGTH3; LENGTH3=$(expr length "${1}")
+  # echo "$LENGTH1 - $LENGTH2 - $LENGTH3"
+  if [ "$LENGTH1" -le "$LENGTH2" ] && [ "$LENGTH1" -le "$LENGTH3" ]; then
+      echo "$LENGTH1";
+  elif [ "$LENGTH2" -le "$LENGTH1" ] && [ "$LENGTH2" -le "$LENGTH3" ]; then
+      echo "$LENGTH2";
+  else
+      echo "$LENGTH3";
+  fi
 }
 export -f real_length
-
-## Repeat Helper
-function repeat_char() {
+## Repeat Helpers
+function repeat_char ()
+{
   local CHAR;  CHAR=${1-'\u2550'};
   local COUNT; COUNT=${2-1};
   local t;     t=$(printf "%-${COUNT}b" "${CHAR}");
@@ -36,3 +48,11 @@ function repeat_char() {
   echo "${t// /${c}}"
 }
 export -f repeat_char
+function repeat_string ()
+{
+  local CHAR;  CHAR=${1-'\u2550'};
+  local COUNT; COUNT=${2-1};
+  local c;     c=( "$(yes "${CHAR}" | head -n ${COUNT})" );
+  echo "${c//$'\n'/ }"
+}
+export -f repeat_string

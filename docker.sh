236222e848191033d1428d35aff920882ec0f4ab
James Zimmerman II <james@impekable.com>
2018-03-01 12:45:07 -0500

    Extract JSON logic and improving overall parsing of help functionality.

--- docker.sh
+++ docker.sh
@@ -3,10 +3,63 @@
 #
 # CONFIG
 ###################
+__USE_DOCKER=false
+DOCKER=""
+COMPOSE=""
+
+#
+# LOAD LIBRARIES
+###################
+require utils
 
 #
 # MODULE LOGIC
 ###################
+## Docker toggle helpers
+function set__use_docker ()
+{
+  dump_method "$@"
+  local value; value=${1-true}; shift
+  __USE_DOCKER=${value}
+}
+export -f set__use_docker
+function get__use_docker ()
+{
+  dump_method "$@"
+  [[ ${__USE_DOCKER} ]] && return 0 || return 1
+}
+export -f get__use_docker
+function use_docker ()
+{
+  return get__use_docker;
+}
+export -f use_docker
+function set__docker ()
+{
+  dump_method "$@"
+  [[ ${__USE_DOCKER} ]] && return 0 || return 1
+}
+export -f set__docker
+## Runtime Configuration
+function configure_docker ()
+{
+  dump_method "$@"
+  # Assemble base docker and compose command
+  COMPOSE=$( assemble_compose ${___COMPOSE[@]} )
+  DOCKER=$( assemble_docker )
+}
+export -f configure_docker
+## Generate a docker compose command
+assemble_compose () {
+  echo $( assemble_command "${_CONFIG_COMPOSECMD}" "-f" $* )
+}
+export -f assemble_compose
+## Generate a docker command
+assemble_docker () {
+  echo $( assemble_command "${_CONFIG_DOCKERCMD}" "-f" $* )
+}
+export -f assemble_docker
+
 #
 # Shutdown, rebuild and run a custom command for the
 # current ${COMPOSE} command string
@@ -27,17 +80,6 @@ reload_config () {
   reload $@
 }
 export -f reload_config
-# Generate a docker compose command
-assemble_compose () {
-  echo $( assemble_command "${_CONFIG_COMPOSECMD}" "-f" $* )
-}
-export -f assemble_compose
-# Generate a docker command
-assemble_docker () {
-  echo $( assemble_command "${_CONFIG_DOCKERCMD}" "-f" $* )
-}
-export -f assemble_docker
-
 select_files () {
   echo $( filter_array __FILES $@ )
 }
@@ -50,8 +92,39 @@ select_containers () {
   echo $( filter_array __CONTAINERS $@ )
 }
 export -f select_containers
+
 #
-# Assemble base docker command
+# ROUTING
+###################
+## Define Module Routes
+route_load "$(cat <<ROUTE
+[
+	{
+	  "namespace": "docker",
+		"use": "--use-docker",
+		"description": "Flag to enable general docker usage.",
+		"callback": "set__use_docker",
+		"priority": 999
+	},
+	{
+	  "namespace": "docker",
+		"use": "--docker (binary)",
+		"description": "Specify the docker binary to use.",
+		"callback": "set__docker",
+		"priority": 999
+	},
+	{
+	  "namespace": "docker",
+		"use": "--compose (binary)",
+		"description": "Specify the docker compose binary to use.",
+		"callback": "set__compose",
+		"priority": 999
+	}
+]
+ROUTE
+)"
+
 #
-COMPOSE=$( assemble_compose ${___COMPOSE[@]} )
-DOCKER=$( assemble_docker )
+# MODULE INIT
+###################
+configure_docker
--- test.sh
+++ test.sh
@@ -25,24 +25,3 @@ not_empty()
   fi
 }
 export -f not_empty
-#
-# Filesystem helpers
-#
-dir_exists()
-{
-  verbose 4 "Directory existence check: $1"
-  if [ -d "$1" ]; then
-    verbose 3 "Directory exists: $1"
-    shift;  $*
-  fi
-}
-export -f dir_exists
-dir_not_exists()
-{
-  verbose 4 "Directory existence check: $1"
-  if [ ! -d "$1" ]; then
-    verbose 3 "Directory does not exist: $1"
-    shift;  $*
-  fi
-}
-export -f dir_not_exists
